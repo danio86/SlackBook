3dcd66c04d137ba91c4282d8da81cd95107757c0
danio86 <d.harder@gmx.at>
2023-03-02 16:21:49 +0000

    add env.py

--- eny.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://oqccbrnk:5462LmUSH4gRelFPOLOSkEeT3RMcPeKE@trumpet.db.elephantsql.com/oqccbrnk"
-os.environ["SECRET_KEY"] = "83tjd-&1txl=v^0zibjrbl06^vzcid@*-+n9#d$-c#pl_bo3wr"
-os.environ["CLOUDINARY_URL"] = "cloudinary://327836945551748:YMPtuzBnuEYN5ijgQulYKZbeZdk@ddigiimwd"
--- slackbook_admin/settings.py
+++ slackbook_admin/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 import dj_database_url
-if os.path.isfile('env.py'):
+if os.path.isfile("env.py"):
     import env
 
-# development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,8 +94,9 @@ WSGI_APPLICATION = 'slackbook_admin.wsgi.application'
 #     }
 # }
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(),
 }
 
 # Password validation
