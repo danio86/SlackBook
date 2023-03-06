dc0c32e0b2035eaf9d23a916c6b87283f440f50c
danio86 <d.harder@gmx.at>
2023-03-06 10:16:50 +0000

    add models to templates and created new model

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+# django has a buildin default User(model) with first name, last name,...
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
@@ -30,4 +31,19 @@ class Channel(models.Model):
     #     return self.likes.count()
 
 
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    # this says to which channel the post belongs
+    # all posts are children of the Channel Class/Model.
+    # If Channel or User gets deleted, all posts have to get deleted too.
+    body = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    # status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.body[0:30]
--- base/views.py
+++ base/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render
+from .models import Channel
 
 
-channels = [
-    {'id': 1, 'name': 'Recipes'},
-    {'id': 2, 'name': 'Politics'},
-    {'id': 3, 'name': 'Social Activities'},
-    {'id': 4, 'name': 'Sport'},
-    {'id': 5, 'name': 'SlackBay'},
-]
+# channels = [
+#     {'id': 1, 'name': 'Recipes'},
+#     {'id': 2, 'name': 'Politics'},
+#     {'id': 3, 'name': 'Social Activities'},
+#     {'id': 4, 'name': 'Sport'},
+#     {'id': 5, 'name': 'SlackBay'},
+# ]
 
 
 # Created views
 def home(request):
-    context = {'channels': channels}
+    queryset = Channel.objects.all()
+    # puts all objects in the model to context to render in index.html
+    context = {'channels': queryset}
     return render(request, 'base/index.html', context)
 
 
 def channel(request, pk):
-    channel = None
-    for i in channels:
-        if i['id'] == int(pk):
-            channel = i
-    context = {'channel': channel}
+    queryset = Channel.objects.get(id=pk)
+    # puts the Channel-object (created in the admin panel) with the id 
+    # into context. django gives all objects a id by default
+    context = {'channel': queryset}
     return render(request, 'base/channel.html', context)
+
+
+# def channel(request, pk):
+#     channel = None
+#     for i in channels:
+#         if i['id'] == int(pk):
+#             channel = i
+#     context = {'channel': channel}
+#     return render(request, 'base/channel.html', context)
